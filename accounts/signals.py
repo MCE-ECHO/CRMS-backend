f577396d581c9ace2482b2f9b2f0cdadf341d572
Rakshak-D <rakshakmce@gmail.com>
2025-04-15 22:38:16 +0530

    Update post_save signal to create Teacher or Student profile based on user type

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,10 +1,12 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import TeacherProfile
+from .models import TeacherProfile, StudentProfile
 
 @receiver(post_save, sender=User)
-def create_teacher_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        TeacherProfile.objects.create(user=instance)
-
+        if instance.is_staff:
+            TeacherProfile.objects.create(user=instance)
+        else:
+            StudentProfile.objects.create(user=instance)
