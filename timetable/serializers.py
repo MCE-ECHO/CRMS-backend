b26f7188afb1e5c0d909387aeda2c0bf4736a984
Rakshak-D <rakshakmce@gmail.com>
2025-04-15 22:56:51 +0530

    Update serializers: Modify Timetable serializer with classroom and teacher read-only fields

--- timetable/serializers.py
+++ timetable/serializers.py
@@ -4,8 +4,8 @@ from .models import Timetable
 class TimetableSerializer(serializers.ModelSerializer):
     teacher = serializers.ReadOnlyField(source='teacher.username')
     classroom = serializers.ReadOnlyField(source='classroom.name')
+    block_name = serializers.ReadOnlyField(source='classroom.block.name')
 
     class Meta:
         model = Timetable
         fields = '__all__'
-
