b4d782ba6d7d2ca68cf0d398b5e92f61e6a35e6b
Rakshak-D <rakshakmce@gmail.com>
2025-04-15 22:38:16 +0530

    Update TeacherProfile and StudentProfile models with avatar, phone, and bio fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,12 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 
 class TeacherProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacherprofile')
     department = models.CharField(max_length=100, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True)
+    bio = models.TextField(blank=True)
 
     def __str__(self):
         return self.user.get_full_name() or self.user.username
@@ -11,3 +14,12 @@ class TeacherProfile(models.Model):
     def is_teacher(self):
         return True
 
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='studentprofile')
+    roll_number = models.CharField(max_length=20, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    phone = models.CharField{max_length=15, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
