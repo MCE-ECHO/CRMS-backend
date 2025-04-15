eaa04eed5414fac24fc67b6a38a947d1ef81aa5e
Rakshak-D <rakshakmce@gmail.com>
2025-04-15 22:52:04 +0530

    Update views: Add classroom list view logic

--- classroom/views.py
+++ classroom/views.py
@@ -2,9 +2,5 @@ from django.shortcuts import render
 from .models import Classroom
 
 def classroom_list(request):
-    """
-    Display a list of classrooms.
-    """
     classrooms = Classroom.objects.all().select_related('block')
     return render(request, 'classroom/classroom_list.html', {'classrooms': classrooms})
-
