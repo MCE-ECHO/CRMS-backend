f8a9626ffd1b08df9dd25a91a6a7f85c2d6f4ad0
Rakshak-D <rakshakmce@gmail.com>
2025-04-15 22:56:51 +0530

    Update models: Modify Timetable model with teacher and classroom relations

--- timetable/models.py
+++ timetable/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from classroom.models import Classroom
+from django.core.exceptions import ValidationError
 
 class Timetable(models.Model):
     day = models.CharField(max_length=10)
@@ -11,6 +12,21 @@ class Timetable(models.Model):
     teacher = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     block_name = models.CharField(max_length=50, blank=True, null=True)
 
+    def clean(self):
+        if self.start_time >= self.end_time:
+            raise ValidationError("End time must be after start time.")
+        conflicts = Timetable.objects.filter(
+            classroom=self.classroom,
+            day=self.day,
+            start_time__lt=self.end_time,
+            end_time__gt=self.start_time
+        ).exclude(id=self.id)
+        if conflicts.exists():
+            raise ValidationError("This time slot is already booked for this classroom.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.day} - {self.start_time} - {self.end_time} - {self.classroom} - {self.teacher}"
-
