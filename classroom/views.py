eba99ecd14d01de42993dd099ae8493abb65cacb
Rakshak-D <rakshakmce@gmail.com>
2025-04-19 15:45:02 +0530

    Add classroom_list view with select_related optimization and docstring

--- classroom/views.py
+++ classroom/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from .models import Classroom
 
 def classroom_list(request):
+    """
+    View to display a list of all classrooms with their associated blocks.
+    """
     classrooms = Classroom.objects.all().select_related('block')
     return render(request, 'classroom/classroom_list.html', {'classrooms': classrooms})
+
