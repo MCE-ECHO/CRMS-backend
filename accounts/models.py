7131e39379609db832a291e0fbed0c7d2367d7f1
Rakshak-D <rakshakmce@gmail.com>
2025-04-19 14:21:06 +0530

    Fix syntax error and clean up models.py in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -18,8 +18,9 @@ class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='studentprofile')
     roll_number = models.CharField(max_length=20, blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-    phone = models.CharField{max_length=15, blank=True)
+    phone = models.CharField(max_length=15, blank=True)  # Fixed syntax error here
     bio = models.TextField(blank=True)
 
     def __str__(self):
         return self.user.get_full_name() or self.user.username
+
