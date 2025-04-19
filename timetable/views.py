208ad2c8b880f94320f15469ec0e1f153d591a32
Rakshak-D <rakshakmce@gmail.com>
2025-04-19 15:50:15 +0530

    Add API views for teacher-specific and all timetables with docstrings and query optimizations

--- timetable/views.py
+++ timetable/views.py
@@ -1,19 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
 from .models import Timetable
 from .serializers import TimetableSerializer
-from django.shortcuts import get_object_or_404
 
 class TeacherTimetableView(APIView):
+    """
+    API view to retrieve timetable entries for a specific teacher.
+    """
     def get(self, request, user_id):
         teacher = get_object_or_404(User, id=user_id, is_staff=True)
         timetable = Timetable.objects.filter(teacher=teacher).select_related('classroom')
         serializer = TimetableSerializer(timetable, many=True)
         return Response(serializer.data)
 
+
 class AllTimetableView(APIView):
+    """
+    API view to retrieve all timetable entries.
+    """
     def get(self, request):
         timetables = Timetable.objects.all().select_related('classroom', 'teacher')
         serializer = TimetableSerializer(timetables, many=True)
         return Response(serializer.data)
+
