7653041bbcafb3053b1d07eb28e806e3e5d1e6a5
Rakshak-D <rakshakmce@gmail.com>
2025-04-19 14:21:35 +0530

    Add post_save signal to auto-create TeacherProfile or StudentProfile on User creation

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,8 +5,13 @@ from .models import TeacherProfile, StudentProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Signal to create a TeacherProfile or StudentProfile
+    automatically when a new User instance is created.
+    """
     if created:
         if instance.is_staff:
             TeacherProfile.objects.create(user=instance)
         else:
             StudentProfile.objects.create(user=instance)
+
