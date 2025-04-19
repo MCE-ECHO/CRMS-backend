8018afdccd11d2ef0d470048d2baa6b5a58b606a
Rakshak-D <rakshakmce@gmail.com>
2025-04-19 14:22:59 +0530

    Add docstrings and clean formatting in utils.py for user role checks

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,5 +1,15 @@
 def is_teacher(user):
+    """
+    Check if the user has an associated TeacherProfile.
+    Returns True if user is a teacher, False otherwise.
+    """
     return hasattr(user, 'teacherprofile')
 
+
 def is_admin(user):
+    """
+    Check if the user has admin privileges.
+    Returns True if user is staff or superuser, False otherwise.
+    """
     return user.is_staff or user.is_superuser
+
