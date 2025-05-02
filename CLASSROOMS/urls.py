72c6d5eff6953c599fab530059af2a6c209bf37e
Rakshak-D <rakshakmce@gmail.com>
2025-05-02 16:54:24 +0000

    Update urls.py: Ensured logout view uses LOGOUT_REDIRECT_URL for consistent redirects

--- CLASSROOMS/urls.py
+++ CLASSROOMS/urls.py
@@ -14,9 +14,8 @@ urlpatterns = [
     path('booking/', include('booking.urls')),
     path('admin-dashboard/', include('admin_dashboard.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
