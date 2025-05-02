0530d8b4f1de664491059d7105fbd0a2da0be95e
Rakshak-D <rakshakmce@gmail.com>
2025-05-02 16:55:37 +0000

    Update admin_dashboard/forms.py: Added Tailwind classes to file input for consistent styling

--- admin_dashboard/forms.py
+++ admin_dashboard/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(widget=forms.FileInput(attrs={'class': 'w-full p-2 border rounded', 'accept': '.csv,.xlsx'}))
