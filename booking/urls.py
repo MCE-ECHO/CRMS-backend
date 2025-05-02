9825d132f9ce2bda087e31b2af5b9f040cd29329
Rakshak-D <rakshakmce@gmail.com>
2025-05-02 16:56:16 +0000

    Update booking/urls.py: Removed ApproveBookingView URL to align with student booking removal

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import BookingListCreateView, ApproveBookingView
+from .views import BookingListCreateView
 
 urlpatterns = [
     path('', BookingListCreateView.as_view(), name='booking-list-create'),
-    path('approve/<int:pk>/', ApproveBookingView.as_view(), name='approve-booking'),
 ]
-
