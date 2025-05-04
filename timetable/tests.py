b95e113c977ac2a313bb0d64ef1f0fce5aa47069
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 17:57:26 +0000

    Add unit tests for timetable app functionality

--- timetable/tests.py
+++ timetable/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Timetable
+from classroom.models import Classroom, Block
 
-# Add tests as needed
-class TimetableTestCase(TestCase):
+class TimetableModelTests(TestCase):
     def setUp(self):
-        pass
+        self.user = User.objects.create_user(username='teacher', password='12345', is_staff=True)
+        self.block = Block.objects.create(name='A Block')
+        self.classroom = Classroom.objects.create(name='CSE101', block=self.block)
+        self.timetable = Timetable.objects.create(
+            classroom=self.classroom,
+            teacher=self.user,
+            day='Monday',
+            start_time='10:00',
+            end_time='11:00',
+            subject_name='Mathematics'
+        )
 
-    def test_placeholder(self):
-        self.assertTrue(True)
+    def test_timetable_str(self):
+        self.assertEqual(str(self.timetable), 'CSE101 - Monday 10:00:00-11:00:00')
