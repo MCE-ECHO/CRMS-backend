6650458309bc3e10327e7c9264250f63513457bc
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 09:56:14 +0000

    Add placeholder tests for admin_dashboard app

--- admin_dashboard/tests.py
+++ admin_dashboard/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Add tests as needed
+class AdminDashboardTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_placeholder(self):
+        self.assertTrue(True)
