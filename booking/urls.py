12ff0dd6d9695780f63999c9c9dbc2d8df4e04db
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 17:55:46 +0000

    Define URL patterns for booking API endpoints

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import BookingListCreateView
 
+app_name = 'booking'
+
 urlpatterns = [
     path('', BookingListCreateView.as_view(), name='booking-list-create'),
 ]
