a730f722d7d040a692d8155c85e9adbf35e56ea6
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 17:56:11 +0000

    Add unit tests for classroom app functionality

--- classroom/tests.py
+++ classroom/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
+from .models import Block, Classroom
 
-# Add tests as needed
-class ClassroomTestCase(TestCase):
+class ClassroomModelTests(TestCase):
     def setUp(self):
-        pass
+        self.block = Block.objects.create(name='A Block')
+        self.classroom = Classroom.objects.create(name='CSE101', block=self.block)
 
-    def test_placeholder(self):
-        self.assertTrue(True)
+    def test_classroom_str(self):
+        self.assertEqual(str(self.classroom), 'CSE101')
