f12d7ceef9b2cb1fbf85ac548e4baed42fb0d8a1
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 13:08:21 +0000

    Add app_name='public_views' to public_views/urls.py and rename public-student-portal to student-portal

--- public_views/urls.py
+++ public_views/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import student_timetable_view, student_available_classrooms, student_portal
 
+app_name='public_views' #Define the namespace
+
 urlpatterns = [
     path('timetable/', student_timetable_view, name='public-timetable'),
     path('available-classrooms/', student_available_classrooms, name='public-available-classrooms'),
