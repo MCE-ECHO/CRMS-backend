02e3f15ed7bd9dc572f6095d009057f395c1ee77
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 09:56:45 +0000

    Add serializers for Booking model

--- booking/serializers.py
+++ booking/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Booking
 
 class BookingSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    classroom = serializers.ReadOnlyField(source='classroom.name')
+
     class Meta:
         model = Booking
         fields = '__all__'
