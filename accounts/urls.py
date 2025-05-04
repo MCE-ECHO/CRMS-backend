dd9ca27a447e7e1f681e55293a3a553bac597484
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 13:06:23 +0000

    Add app_name='accounts' to accounts/urls.py to fix NoReverseMatch error

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from .views import (
     profile_view,
     booking_create_view,  # Added for booking creation
 )
-
+app_name='accounts' #Define the namespace
 urlpatterns = [
     path('teacher/dashboard/', teacher_dashboard, name='teacher-dashboard'),
     path('profile/', profile_view, name='profile'),
