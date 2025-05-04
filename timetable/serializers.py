d94614cece4969ad9ffe54c5136f00989a892ca3
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 17:57:20 +0000

    Add serializers for Timetable model API interactions

--- timetable/serializers.py
+++ timetable/serializers.py
@@ -2,10 +2,6 @@ from rest_framework import serializers
 from .models import Timetable
 
 class TimetableSerializer(serializers.ModelSerializer):
-    teacher = serializers.ReadOnlyField(source='teacher.username')
-    classroom = serializers.ReadOnlyField(source='classroom.name')
-    block_name = serializers.ReadOnlyField(source='classroom.block.name')
-
     class Meta:
         model = Timetable
         fields = '__all__'
