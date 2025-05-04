46fd4a5f492d287d70128780c84f23bdd153aa3c
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 17:57:32 +0000

    Define URL patterns for timetable API endpoints

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import TeacherTimetableView, AllTimetableView
 
+app_name = 'timetable'
+
 urlpatterns = [
     path('teacher/<int:user_id>/', TeacherTimetableView.as_view(), name='teacher-timetable'),
     path('', AllTimetableView.as_view(), name='all-timetables'),
