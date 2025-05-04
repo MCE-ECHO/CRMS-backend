464a29bf2e8c9df3ebf3fce5d600b86d928a36de
Rakshak-D <rakshakmce@gmail.com>
2025-05-04 09:54:58 +0000

    Add signals for automatic profile creation

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,8 +5,9 @@ from .models import TeacherProfile, StudentProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    # Automatically create a profile when a user is created
     if created:
-        if instance.is_staff:
+        if instance.is_staff or instance.is_superuser:
             TeacherProfile.objects.create(user=instance)
         else:
             StudentProfile.objects.create(user=instance)
