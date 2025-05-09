e94af90e185386f5c58cb1e126f0962f6212e9fb
Rakshak-D <rakshakmce@gmail.com>
2025-05-09 20:27:07 +0000

    Update timetable/serializers.py: update timetable serializers

--- timetable/serializers.py
+++ timetable/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Timetable
+from classroom.models import Classroom
+from django.contrib.auth.models import User
 
 class TimetableSerializer(serializers.ModelSerializer):
+    classroom = serializers.PrimaryKeyRelatedField(queryset=Classroom.objects.all())
+    teacher = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(is_staff=True))
+    start_time = serializers.TimeField(format='%H:%M')
+    end_time = serializers.TimeField(format='%H:%M')
+
     class Meta:
         model = Timetable
         fields = '__all__'
+
