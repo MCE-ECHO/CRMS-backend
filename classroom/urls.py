b9e55cfd7f8a7f9496d4a24d2b0cb1d7b4eb6860
Rakshak-D <rakshakmce@gmail.com>
2025-05-09 20:27:04 +0000

    Update classroom/urls.py: update classroom URLs

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views
+from .views import classroom_list_view, classroom_list, available_classrooms
 
 app_name = 'classroom'
 
 urlpatterns = [
-    path('', views.classroom_list, name='classroom-list'),
+    path('list/', classroom_list_view, name='classroom-list'),
+    path('api/list/', classroom_list, name='api-classroom-list'),
+    path('api/available/', available_classrooms, name='api-available-classrooms'),
 ]
+
