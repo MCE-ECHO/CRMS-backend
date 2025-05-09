95af863c41854ede9d718fad789bade3405f8891
Rakshak-D <rakshakmce@gmail.com>
2025-05-09 20:27:04 +0000

    Update booking/serializers.py: update booking serializers

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Booking
+from classroom.models import Classroom
+from django.contrib.auth.models import User
 
 class BookingSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
+    classroom = serializers.SlugRelatedField(slug_field='name', queryset=Classroom.objects.all())
+    start_time = serializers.TimeField(format='%H:%M')
+    end_time = serializers.TimeField(format='%H:%M')
+
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = ['id', 'user', 'classroom', 'date', 'start_time', 'end_time', 'status', 'created_at']
+
