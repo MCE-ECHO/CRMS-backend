21d57672fc9f2ca7a909a7df57f499620ac18b32
Rakshak-D <rakshakmce@gmail.com>
2025-05-09 20:27:04 +0000

    Update admin_dashboard/forms.py: refine admin forms

--- admin_dashboard/forms.py
+++ admin_dashboard/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from accounts.models import Event
 
-class UploadFileForm(forms.Form):
-    file = forms.FileField(
-        label='Select a CSV or Excel file',
-        widget=forms.FileInput(attrs={'accept': '.csv,.xlsx', 'class': 'w-full p-3 border rounded-lg', 'id': 'fileInput'})
-    )
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['title', 'description', 'start_date', 'end_date']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'w-full p-3 border rounded-lg'}),
+            'description': forms.Textarea(attrs={'class': 'w-full p-3 border rounded-lg', 'rows': 4}),
+            'start_date': forms.DateTimeInput(attrs={'class': 'w-full p-3 border rounded-lg', 'type': 'datetime-local'}),
+            'end_date': forms.DateTimeInput(attrs={'class': 'w-full p-3 border rounded-lg', 'type': 'datetime-local'}),
+        }
+
