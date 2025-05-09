e35c1c91ca46fcacc79c9d7c45d569d637c17af1
Rakshak-D <rakshakmce@gmail.com>
2025-05-09 20:27:05 +0000

    Update utils.js: add/fix JS utility functions

--- static/js/utils.js
+++ static/js/utils.js
@@ -4,5 +4,32 @@ function handleError(error, elementId, message) {
     if (element) {
         element.innerHTML = `<p class="text-red-500">${message}</p>`;
     }
-    Swal.fire('Error', message, 'error');
+    Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: message,
+        confirmButtonColor: '#0071E3'
+    });
 }
+
+function ajaxGet(url, callback) {
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    })
+        .then(response => response.json())
+        .then(data => callback(null, data))
+        .catch(error => callback(error, null));
+}
+
+function serializeForm(form) {
+    const formData = new FormData(form);
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+    return data;
+}
+
