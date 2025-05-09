380463a67a3a064af472470ec9ba47b40fd90e5e
Rakshak-D <rakshakmce@gmail.com>
2025-05-09 20:27:04 +0000

    Update booking/urls.py: update booking URLs

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import BookingListCreateView
+from .views import (
+    booking_create_view, booking_list_view, BookingListView, approve_booking, reject_booking
+)
 
 app_name = 'booking'
 
 urlpatterns = [
-    path('', BookingListCreateView.as_view(), name='booking-list-create'),
+    path('create/', booking_create_view, name='booking-create'),
+    path('list/', booking_list_view, name='booking-list'),
+    path('admin/list/', BookingListView.as_view(), name='admin-booking-list'),
+    path('admin/approve/<int:pk>/', approve_booking, name='approve-booking'),
+    path('admin/reject/<int:pk>/', reject_booking, name='reject-booking'),
 ]
+
