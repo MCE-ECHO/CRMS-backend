40393a2ce05e5d94ada71ea5e60c17fcf5a33b1b
Rakshak-D <rakshakmce@gmail.com>
2025-05-11 00:39:23 +0530

    Refactor EventForm: add visibility and better widgets

--- admin_dashboard/forms.py
+++ admin_dashboard/forms.py
@@ -4,11 +4,11 @@ from accounts.models import Event
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'description', 'start_date', 'end_date']
+        fields = ['title', 'start_date', 'end_date', 'visibility']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'w-full p-3 border rounded-lg'}),
-            'description': forms.Textarea(attrs={'class': 'w-full p-3 border rounded-lg', 'rows': 4}),
-            'start_date': forms.DateTimeInput(attrs={'class': 'w-full p-3 border rounded-lg', 'type': 'datetime-local'}),
-            'end_date': forms.DateTimeInput(attrs={'class': 'w-full p-3 border rounded-lg', 'type': 'datetime-local'}),
+            'start_date': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'w-full p-3 border rounded-lg'}),
+            'end_date': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'w-full p-3 border rounded-lg'}),
+            'visibility': forms.Select(attrs={'class': 'w-full p-3 border rounded-lg'}),
         }
 
