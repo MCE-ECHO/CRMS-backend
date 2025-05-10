7e12fde7ddeb71620e8c717a82a65e63cdba677c
Rakshak-D <rakshakmce@gmail.com>
2025-05-11 00:35:14 +0530

    Add public views test for student portal access

--- public_views/tests.py
+++ public_views/tests.py
@@ -2,5 +2,6 @@ from django.test import TestCase
 
 class PublicViewsTests(TestCase):
     def test_student_portal_access(self):
-        response = self.client.get('/public/portal/')
+        response = self.client.get('/public/student-portal/')
         self.assertEqual(response.status_code, 200)
+
