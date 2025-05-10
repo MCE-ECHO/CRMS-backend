1ed50e71034f1217a54d063eade6982d8d3b8684
Rakshak-D <rakshakmce@gmail.com>
2025-05-11 00:39:25 +0530

    Update timetable serializers: add classroom and teacher info

--- timetable/serializers.py
+++ timetable/serializers.py
@@ -4,12 +4,11 @@ from classroom.models import Classroom
 from django.contrib.auth.models import User
 
 class TimetableSerializer(serializers.ModelSerializer):
-    classroom = serializers.PrimaryKeyRelatedField(queryset=Classroom.objects.all())
-    teacher = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(is_staff=True))
+    classroom = serializers.SlugRelatedField(slug_field='name', queryset=Classroom.objects.all())
+    teacher = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
     start_time = serializers.TimeField(format='%H:%M')
     end_time = serializers.TimeField(format='%H:%M')
 
     class Meta:
         model = Timetable
-        fields = '__all__'
-
+        fields = ['id', 'day', 'start_time', 'end_time', 'classroom', 'teacher', 'subject_name']
