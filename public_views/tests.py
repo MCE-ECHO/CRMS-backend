913dbadc202f8cd56b4ddd5123f8336fce1688ec
Rakshak-D <rakshakmce@gmail.com>
2025-05-11 14:11:45 +0530

    Add test in tests.py for student portal access

--- public_views/tests.py
+++ public_views/tests.py
@@ -4,4 +4,3 @@ class PublicViewsTests(TestCase):
     def test_student_portal_access(self):
         response = self.client.get('/public/student-portal/')
         self.assertEqual(response.status_code, 200)
-
